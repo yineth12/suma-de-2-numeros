a2e44cc43d20cac0a23368cb45de97623507493c
Yinel Ayala <>
2021-04-27 04:05:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function operacion(num1,num2){
+let op= num1+num2;
+console.log("la suma es", op);
+
+}
+
+let numero1 = parseInt(prompt("digame el primer numero"));
+let numero2 = parseInt(prompt("digame el segundo numero"));
+
+operacion(numero1,numero2)
